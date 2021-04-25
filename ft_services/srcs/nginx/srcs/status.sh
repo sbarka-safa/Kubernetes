93a3a4daf541f87980fecbf04f3bc9638d611696
sbarka-safa <sbarka@e1r11p2.1337.ma>
2021-04-25 17:40:59 +0000

    verify nginx deployment

--- ft_services/srcs/nginx/srcs/status.sh
+++ ft_services/srcs/nginx/srcs/status.sh
@@ -5,14 +5,14 @@ v_telegraf=$?
 pgrep nginx
 v_nginx=$?
 
-pgerp sshd
+pgrep sshd
 v_sshd=$?
 
 ps > crashed
 grep '\[*\]' crashed
 v_crashed=$?
 
-if [ $v_telegraf == 0 ] && [ $v_crashed == 1 ] && [ $v_nginx == 0 ] && [$v_sshd == 0]; then
+if [ $v_telegraf == 0 ] && [ $v_crashed == 1 ] && [ $v_nginx == 0 ] &&[ $v_sshd == 0 ]; then
         exit 0;
 fi
 exit 1;
