30548eb3798803fbe5b11033a652c490c8231e4d
sbarka-safa <sbarka@e1r11p2.1337.ma>
2021-04-26 13:03:51 +0000

    check phpmyadmin and wordpress deployments

--- ft_services/srcs/wordpress/srcs/status.sh
+++ ft_services/srcs/wordpress/srcs/status.sh
@@ -1,6 +1,9 @@
 pgrep nginx
 v_nginx=$?
 
+pgrep php-fpm7
+v_php=$?
+
 pgrep telegraf
 v_telegraf=$?
 
@@ -8,7 +11,7 @@ ps > crashed
 grep '\[*\]' crashed
 v_crashed=$?
 
-if [ $v_telegraf == 0 ] && [ $v_crashed == 1 ] && [ $v_nginx == 0 ]; then
+if [ $v_telegraf == 0 ] && [ $v_crashed == 1 ] && [ $v_nginx == 0 ] && [ $v_php == 0 ]; then
         exit 0;
 fi
 exit 1;
